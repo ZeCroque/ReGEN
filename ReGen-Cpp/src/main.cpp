4ddc8f9cc9d3b0baba6e536c29f8c1217fdcb8f4
ZeCroque <zobilezeub@gmail.com>
2021-07-21 01:41:02 +0200

    Misc->Updated .gitignore Config->Added xml parsing library WIP->Added xml parsing TestCode

--- ReGen-Cpp/src/main.cpp
+++ ReGen-Cpp/src/main.cpp
@@ -1,4 +1,22 @@
+#include "pugixml.hpp"
+#include <iostream>
+
 int main()
 {
-    return 0;
+    pugi::xml_document doc;
+	
+    doc.load_file("Data/Rules/InitializationRules/Assassinate/Assassinate_Social_Condition.xml");
+        
+    for (const pugi::xml_node& node : doc.child("graph").child("nodes").children("node"))
+    {
+        std::cout << "Name : \"" << node.attribute("name").as_string() << "\"" << std::endl;
+
+    	for(const pugi::xml_node& condition : node.children("attr"))
+    	{
+    		std::cout << "Condition : \"" << condition.attribute("name").as_string() << "\" of type \"" << condition.attribute("type").as_string() << "\" equals \"" << condition.child("value").text().as_string() << "\"" << std::endl;
+    	}
+         std::cout << "================================" << std::endl;
+    }
+
+	return 0;
 }
